d7d0bc72205d54ed5b649541bccb6fcaf2807fd5
Eric Wilsey <eric.wilsey@umconnect.umt.edu>
2017-10-04 13:17:05 -0600

    Re-attempting to commit hw-3 as the link would not work on the Git-Hub Repository for the final part of the assignment.

---
+++ hw-3/empty-example/sketch.js
@@ -0,0 +1,18 @@
+// Declare the setup function
+function setup() {
+
+    // create a canvas 600px wide and 400px high
+    createCanvas( 900, 600 );
+
+    // color the background 'pink'
+    background( 'magenta' );
+}
+
+
+// Declare the draw function
+function draw() {
+
+    // tell p5 to print the text string "Hello World!"
+    // This is going to be positioned at ( x: 20, y: 30 ).
+    text( "Hello World!", 10, 10 );
+}
