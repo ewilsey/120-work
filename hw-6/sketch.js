9afe16179a7f56af3de9634099369e723ddd5dc0
Eric Wilsey <eric.wilsey@umconnect.umt.edu>
2017-10-15 17:57:13 -0600

    started build, put background in setup, declared global variables, testing variables and constraints.  Implementing random.

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,13 +1,33 @@
+// Global variables
+var cc; //canvas color
+
+
 //setup
 function setup() {
+  cc=color( 'rgb(134, 27, 27)' );
   createCanvas( windowWidth, windowHeight );
-  background( 25, 25, 25 );
+  background( cc );
 }
 // variables
 
 
 // draw
 function draw() {
+  var borderL = 0;
+  var borderR = width -2;
+  var borderC = 0;
+  var borderF = height -2;
+  // show borders.
+
+  rect( borderL ++, 0, borderL ++, height ); // (pos left/right, pos up/down, width, height)
+  rect( borderR --, 0, borderR --, height );
+  rect( 0, borderC, width, borderC );
+  rect( 0, borderF, width, borderF );
+  noFill();
+  stroke( random(255), random(255), random(255) );
+
+  var cSize = random( 10, width-20 );
+  ellipse( width/4, height/4, cSize );
 
 
 }
