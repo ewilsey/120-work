15c90482f6dabc8840a6a115022611d6bf2b2ef8
Eric Wilsey <eric.wilsey@umconnect.umt.edu>
2017-10-15 19:50:10 -0600

    Added a ton of random features, may dial them back some. Background will generate 1 random color, borders flash random colors, and ellipses are all colored randomly.

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -4,9 +4,10 @@ var cc; //canvas color
 
 //setup
 function setup() {
-  cc=color( 'rgb(134, 27, 27)' );
+  cc=color( random(55), random(55), random(55) );// generates 1 random darkened background.
   createCanvas( windowWidth, windowHeight );
   background( cc );
+
 }
 // variables
 
@@ -17,17 +18,25 @@ function draw() {
   var borderR = width -2;
   var borderC = 0;
   var borderF = height -2;
-  // show borders.
 
-  rect( borderL ++, 0, borderL ++, height ); // (pos left/right, pos up/down, width, height)
-  rect( borderR --, 0, borderR --, height );
-  rect( 0, borderC, width, borderC );
-  rect( 0, borderF, width, borderF );
+
+  push();   // start ellipse animation and create animated borders.
+  frameRate(5);
   noFill();
-  stroke( random(255), random(255), random(255) );
+  stroke( random(255), random(255), random(255) );//rainbow borders
+  rect( borderL ++, 0, borderL ++, height ); // left (pos left/right, pos up/down, width, height)
+  rect( borderR --, 0, borderR --, height ); // right
+  rect( 0, borderC, width, borderC ); //top
+  rect( 0, borderF, width, borderF ); //bottom
+  noFill();
+  stroke( random(255), random(255), random(255) );//color ellipse
 
-  var cSize = random( 10, width-20 );
-  ellipse( width/4, height/4, cSize );
+
+  var cSize = random( 10, width-20 ); //randomize size
+  ellipse( random(width/2), random(height/2), cSize ); //random placement + random size
+  pop(); // end ellipse animation
+
+
 
 
 }
