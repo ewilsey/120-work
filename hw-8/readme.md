d9147c29a1c9caf0e7dcb56480a1a64f91dd1e7f
Eric Wilsey <eric.wilsey@umconnect.umt.edu>
2017-11-01 22:50:08 -0600

    github wouldn't open from Atom, had to open from control panel, then my code that I made's index file wouldn't work, and it was based on Shiffman vid, then I copied his word for word, still nothing. Then I copied other examples, they worked, don't know what's going on.

---
+++ hw-8/readme.md
@@ -0,0 +1,13 @@
+ERIC WILSEY, 51
+
+[Live Sketch Link](https://ewilsey.github.io/120-work/hw-8/)
+
+# HW 8
+
+## Description
+
+<!--
+
+
+
+-->
---
+++ hw-8/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+      createCanvas( windowWidth -20 , windowHeight -20 );
+}
+
+function draw(){
+  background(0);
+  strokeWeight(4);
+  stroke(255);
+
+}
